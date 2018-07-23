6011b481ee16e387afed5af8ad27177e06d192a4
Tomassagustin32 <40763567+Tomassagustin32@users.noreply.github.com>
2018-07-23 12:20:55 -0300

    Ferrete iluminación

---
+++ jsFerreteIluminacion.js
@@ -0,0 +1,122 @@
+/*4.	Para el departamento de iluminación:
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+ ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
+
+ */
+function CalcularPrecio () 
+{
+	var cantidad;
+	var marca;
+	var precio;
+	var descuento;
+	var precioFinal;
+ 	var precioBruto;
+
+ 	precio=35;
+ 	
+ 	cantidad=document.getElementById('Cantidad').value;
+ 	cantidad=parseInt(cantidad);
+ 	
+ 	marca=document.getElementById('Marca').value;
+ 	
+ 	precioBruto=35*cantidad;
+/*
+ 	if (cantidad >5)
+ 	{
+ 		//50
+ 	}
+ 	else 
+ 	{
+ 		if(cantidad == 5 && marca == "ArgentinaLuz")
+ 		{
+ 			//40
+ 		}
+ 		else
+ 		{
+            //30
+ 			if(cantidad == 4 && marca == "ArgentinaLuz" || "FelipeLamparas")
+ 			{
+ 				//25
+ 			}
+ 			else 
+ 			{
+ 				//20
+ 				if(cantidad == 3 && marca)
+ 				{
+ 					//15
+ 				}
+ 				if else 
+ 				{
+ 					//10
+ 				}
+ 				else 
+ 				{
+ 					//5
+ 				}
+ 			}  
+ 		}
+ 	}
+*/
+
+	switch (cantidad)
+	{
+		case 1:
+		case 2:
+			break;
+		case 3:
+			switch (marca)
+			{
+				case "ArgentinaLuz":
+					descuento=0.85;
+					
+					break;
+				case "FelipeLamparas":
+					descuento=0.90;
+					
+					break;
+					default:
+						descuento=0.95;
+			}
+			break;
+		case 4:
+				switch (marca)
+				{
+					case "ArgentinaLuz":
+					case "FelipeLamparas":
+						descuento=0.75;
+						break;
+						default:
+							descuento=0.80;	
+				}
+		case 5:
+					switch (marca)
+					{
+						case "ArgentinaLuz":
+							descuento=0.60;
+							break;
+							default:
+								descuento=0.70;
+					}
+		default:
+			descuento=0.50;
+
+	}
+precioFinal=descuento*precioBruto;
+document.getElementById('precioDescuento').value=precioFinal;
+}
+/*if(cant)
+if(marca) (((1 FORMA)))
+
+switch
+	switch (((2 FORMA)))
+
+if ()
+	switch (((3 FORMA)))
+
+Switch
+	if    (((4 FORMA)))		
