c735f33369cb64dfa6259bef2a9ca65169fcb96d
Tomassagustin32 <40763567+Tomassagustin32@users.noreply.github.com>
2018-08-01 03:44:40 -0300

    siete a medias

--- siete.js
+++ siete.js
@@ -1,85 +1,74 @@
-/* 
-ingreso:
-maria f 15
-jose m 33
-pepe m 81
-
-mostrar: 
-	cantidad:
-		1-de mayores de edad= 2 
-		2-menores de edad= 1
-		3-mujeres = 1
-		4-hombres = 2
-		5-nombre mayor= pepe
-		6-nombre menor= maria
-		7-nombre hombre menor edad= jose
-
-*/
 function mostrar()
 {
-	var hombreMenor;
-	var mujerMenor;
-	var mujerMayor;
-	var hombreMayor;
-	var contadorMayor;
-	var contadorMenor;
-	var contadorHombres;
-	var contadorMujeres;
+	var hombres;
+	var mujeres;
 	var contador;
-	var nombre;
-	var edad;
 	var sexo;
+	var edad;
+	var mayor=0;
+	var menor=0;
+	var nombre;
+	var nombreMayor;
+	var nombreMenor;
 
+	bandera=0
 	contador=0;
-	contadorHombres=0;
-	contadorMujeres=0;
-	contadorMayor=0;
-	contadorMenor=0;
-	
-	while(contador <3)
+	mujeres=0;
+	hombres=0;
+
+	while(contador<3)
 	{
 		contador++;
-		nombre=prompt("Ingrese los nombres");
-		
-		edad=prompt("ingrese edad");
-		edad=parseInt(edad);
-		while(edad <0 || edad >100)
-		{
-			edad=prompt("Ingrese las edades");
-			edad=parseInt(edad);
+		nombre=prompt("Ingrese su nombre #"+contador);
+		sexo=prompt("Ingrese su sexo (f/m) #"+contador);
+		edad=parseInt(prompt("Ingrese su edad #"+contador));
 
-		}
-		
-		sexo=prompt("ingrese sexo (f/m)");
-		while(sexo !="f" && sexo !="m")
+		while(sexo!="f" && sexo!="m")
 		{
-			sexo=prompt("error.Ingrese sexo (f/m");
+			sexo=prompt("ERROR.Ingrese f o m");
 			
 		}
-			if (edad > 17)
+		while(edad<0 || edad>100)
+		{
+			edad=parseInt(prompt("ERROR.Ingrese una edad valida"));
+		}
+			if(sexo=="f")
 			{
-				contadorMayor++;
-			}else
-	{
-				contadorMenor++;
+				mujeres++;
 			}
+			if(sexo=="m")
+			{
+				hombres++;
+			}
+			if(edad<18)
+			{
+				menor++;
+			}
+			if(edad>17)
+			{
+				mayor++;
+			}	
+			if(bandera==0)
+			{
+				nombreMenor=edad;
+				nombreMayor=edad;
 
-			if (sexo == "f")
-			{
-				contadorMujeres++;
 			}
-			if (sexo == "m")
+			if(nombreMayor>edad)
 			{
-				contadorHombres++;
+				edad=nombreMayor;
+			}
+			if(nombreMenor<edad)
+			{
+				edad=nombreMenor;
 			}
 			
-
-	
+			
 	}
-	
-
-	document.write("<br> la cantidad de mayores es : "+contadorMayor);
-	document.write("<br> la cantidad de menores es : "+contadorMenor);
-	document.write("<br> la cantidad de hombres es : "+contadorHombres);
-	document.write("<br> la cantidad de mujeres es : "+contadorMujeres);
+	document.write("<br> la cantidad de mayores es : "+mayor);
+	document.write("<br> la cantidad de menores es : "+menor);
+	document.write("<br> la cantidad de hombres es : "+hombres);
+	document.write("<br> la cantidad de mujeres es : "+mujeres);
+	document.write("<br> el nombre del mayor es : "+nombreMayor);
+	document.write("<br> el nombre del menor es : "+nombreMenor);
 }
